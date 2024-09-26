3912fdad617cc4372af5721e4288055cc2c51f42
Samuel <130667350+SamuelBarbosa1@users.noreply.github.com>
2024-09-26 14:00:00 -0300

    C#

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -2,9 +2,9 @@
 
 class Program {
     static void Main(string[] args) {
-         // Exercicio1.Run();  // Descomente para testar o exercício 2
-         Exercicio2.Run();  // Descomente para testar o exercício 2
-        // Exercicio3.Run();  // Descomente para testar o exercício 3
+         // Exercicio1.Run();  //  o exercício 2
+         Exercicio2.Run();  //  testar o exercício 2
+        // Exercicio3.Run();  // testar o exercício 3
         //Exercicio4.Run();  // Testando o exercício 4
     }
 }
